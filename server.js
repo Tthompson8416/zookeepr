6db5e8df184e615d3203bd95b94666c86e5176b4
TT84 <tthompson8416@outlook.com>
2022-02-20 23:02:38 -0500

    accessing the query property on req object

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const express = require('express');
 const app = express();
 
 app.get('/api/animals', (req, res) => {
-    res.send('Hello');
+    res.json(animals);
 });
 app.listen(3001, () => {
     console.log('API server now on port 3001!')
+});
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    console.log(req.query)
+    res.json(results);
 });
 
