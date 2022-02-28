605a160871f75a3d3a6d9fd3da6905cb050ce22d
TT84 <tthompson8416@outlook.com>
2022-02-27 21:14:16 -0500

    Adding res.sendFile

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ function createNewAnimal(body, animalsArray) {
     animalsArray.push(animal);
     fs.writeFileSync(
         path.join(__dirname, './data/animals.json'),
-        JSON.stringify({animals: animalsArray }, null, 2)
+        JSON.stringify({ animals: animalsArray }, null, 2)
     );
 
     return animal;
@@ -69,19 +69,19 @@ function createNewAnimal(body, animalsArray) {
 
 function validateAnimal(animal) {
     if (!animal.name || typeof animal.name !== 'string') {
-      return false;
+        return false;
     }
     if (!animal.species || typeof animal.species !== 'string') {
-      return false;
+        return false;
     }
     if (!animal.diet || typeof animal.diet !== 'string') {
-      return false;
+        return false;
     }
     if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-      return false;
+        return false;
     }
     return true;
-  }
+}
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -104,15 +104,21 @@ app.get('/api/animals/:id', (req, res) => {
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
-  
+
     // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
-      res.status(400).send('The animal is not properly formatted.');
+        res.status(400).send('The animal is not properly formatted.');
     } else {
-      const animal = createNewAnimal(req.body, animals);
-      res.json(animal);
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
     }
-  });
+});
+
+// GET rout to display HTML page in browser
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
